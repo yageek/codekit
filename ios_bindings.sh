a068bf3235b964779bc4f97636bcb81099d784a5
Yannick Heinrich <yannick.heinrich@gmail.com>
2023-03-03 10:38:06 +0100

    universal done right

--- ios_bindings.sh
+++ ios_bindings.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 LIBRARY_NAME=libcodekit_core
 
+## See https://developer.apple.com/forums/thread/711294 for information related to platforms
+
 ## Run cbindgen
 echo "Building headers ⚙️"
 cd codekit-core
@@ -13,38 +15,39 @@ then
     cargo build --release --target=aarch64-apple-ios --features ffi-interface
     cargo build --release --target=x86_64-apple-ios --features ffi-interface
     cargo build --release --target=aarch64-apple-ios-sim --features ffi-interface
+    cargo build --release --target=aarch64-apple-darwin --features ffi-interface
+    cargo build --release --target=x86_64-apple-darwin --features ffi-interface
 
+    # Now we need to use lipo to merge the two libraries for macOS
+    mkdir -p target/universal-apple-darwin/release
+    lipo -create -output ./target/universal-apple-darwin/release/${LIBRARY_NAME}.a ./target/x86_64-apple-darwin/release/${LIBRARY_NAME}.a ./target/aarch64-apple-darwin/release/${LIBRARY_NAME}.a
     # Create Apple Silicon based version
-    xcodebuild -create-xcframework -output ../bindings/${LIBRARY_NAME}/lib/iOS/release/AppleSilicon/CodeKitCore.xcframework \
+    xcodebuild -create-xcframework -output ../bindings/${LIBRARY_NAME}/lib/Apple/release/CodeKitCore.xcframework \
             -library ./target/aarch64-apple-ios/release/${LIBRARY_NAME}.a \
             -headers ../bindings/libcodekit_core/include \
             -library ./target/aarch64-apple-ios-sim/release/${LIBRARY_NAME}.a \
+            -headers ../bindings/libcodekit_core/include \
+            -library ./target/universal-apple-darwin/release/${LIBRARY_NAME}.a \
             -headers ../bindings/libcodekit_core/include
-
-    # Create Intel based version
-    xcodebuild -create-xcframework -output ../bindings/${LIBRARY_NAME}/lib/iOS/release/Intel/CodeKitCore.xcframework \
-        -library ./target/aarch64-apple-ios/release/${LIBRARY_NAME}.a \
-        -headers ../bindings/libcodekit_core/include \
-        -library ./target/x86_64-apple-ios/release/${LIBRARY_NAME}.a \
-        -headers ../bindings/libcodekit_core/include
 else
     echo "Building debug"
     
     cargo build --target=aarch64-apple-ios --features ffi-interface
     cargo build --target=x86_64-apple-ios --features ffi-interface
     cargo build --target=aarch64-apple-ios-sim --features ffi-interface
-
+    cargo build --target=aarch64-apple-darwin --features ffi-interface
+    cargo build --target=x86_64-apple-darwin --features ffi-interface
+    
+    # Now we need to use lipo to merge the two libraries for macOS
+    mkdir -p target/universal-apple-darwin/debug
+    lipo -create -output ./target/universal-apple-darwin/debug/${LIBRARY_NAME}.a ./target/x86_64-apple-darwin/debug/${LIBRARY_NAME}.a ./target/aarch64-apple-darwin/debug/${LIBRARY_NAME}.a
     # Apple Silicon
-    xcodebuild -create-xcframework -output ../bindings/${LIBRARY_NAME}/lib/iOS/debug/AppleSilicon/CodeKitCore.xcframework \
+    xcodebuild -create-xcframework -output ../bindings/${LIBRARY_NAME}/lib/Apple/debug/CodeKitCore.xcframework \
                 -library ./target/aarch64-apple-ios/debug/${LIBRARY_NAME}.a \
                 -headers ../bindings/libcodekit_core/include \
                 -library ./target/aarch64-apple-ios-sim/debug/${LIBRARY_NAME}.a \
+                -headers ../bindings/libcodekit_core/include \
+                -library ./target/universal-apple-darwin/debug/${LIBRARY_NAME}.a \
                 -headers ../bindings/libcodekit_core/include
 
-    # Intel
-    xcodebuild -create-xcframework -output ../bindings/${LIBRARY_NAME}/lib/iOS/debug/Intel/CodeKitCore.xcframework \
-                -library ./target/aarch64-apple-ios/debug/${LIBRARY_NAME}.a \
-                -headers ../bindings/libcodekit_core/include \
-                -library ./target/x86_64-apple-ios/debug/${LIBRARY_NAME}.a \
-                -headers ../bindings/libcodekit_core/include
 fi
---
+++ ios_create_xcframework.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+export FRAMEWORK_NAME=CodeKit
+cd bindings/CodeKit
+
+# Build for iOS
+xcodebuild archive \
+    -project CodeKit.xcodeproj \
+    -scheme ${FRAMEWORK_NAME}-iOS \
+    -destination "generic/platform=iOS" \
+    -archivePath "archives/${FRAMEWORK_NAME}-iOS"
+
+xcodebuild archive \
+    -project CodeKit.xcodeproj \
+    -scheme ${FRAMEWORK_NAME}-iOS \
+    -destination "generic/platform=iOS Simulator" \
+    -archivePath "archives/${FRAMEWORK_NAME}-iOS_Simulator"
+
+xcodebuild archive \
+    -project CodeKit.xcodeproj \
+    -scheme ${FRAMEWORK_NAME}-macOS \
+    -destination "generic/platform=macOS" \
+    -archivePath "archives/${FRAMEWORK_NAME}-macOS"
+
+xcodebuild -create-xcframework
+    -archive archives/${FRAMEWORK_NAME}-iOS.xcarchive -framework ${FRAMEWORK_NAME}.framework
+    -archive archives/${FRAMEWORK_NAME}-iOS_Simulator.xcarchive -framework ${FRAMEWORK_NAME}.framework
+    -archive archives/${FRAMEWORK_NAME}-macOS.xcarchive -framework ${FRAMEWORK_NAME}.framework
+    -output xcframeworks/${FRAMEWORK_NAME}.xcframework
