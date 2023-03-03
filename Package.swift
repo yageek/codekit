3bd5b2c906b2018c5567ec6d48c42ed5cdfb7377
Yannick Heinrich <yannick.heinrich@gmail.com>
2023-03-03 10:38:47 +0100

    Fix package.swift

--- Package.swift
+++ Package.swift
@@ -2,25 +2,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "RCKCodeKit",
+    name: "CodeKit",
     platforms: [
         .macOS(.v10_13), .iOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "RCKCodeKit",
-            targets: ["RCKCodeKit"])
+            name: "CodeKit",
+            targets: ["CodeKit"])
     ],
     dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "RCKCodeKit"
-        ),
         .binaryTarget(
-            name: "RCKCodeKit",
+            name: "CodeKit",
             url: "https://github.com/yageek/codekit/releases/download/v1.0.0/CodeKit-1.0.0.zip",
             checksum: "69208562f28b68f966166a1b3127bacde3b28d6e0e138fcd91016f7f12cffa73"
         )
